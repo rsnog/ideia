20f5e54254052a376ff4262a6cc859f667374317
Roberto Nogueira <rsnog@hotmail.com>
2020-12-01 15:38:11 -0300

    import Link

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 function Home() {
     return <div>
-        <h1>Home</h1>
+        <h1>Sobre</h1>
 
-        <Link href="/sobre">
-            <a>Acessar página sobre</a>
+        <Link href="/">
+            <a>Acessar página inicial</a>
         </Link>
-
+        
     </div>
 }
 
